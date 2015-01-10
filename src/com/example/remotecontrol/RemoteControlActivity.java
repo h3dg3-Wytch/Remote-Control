0b9f31003b4a37e99dcd94b38f9a5f8c2a637bca
Rex Petersen <rexpeters3n@gmail.com>
2015-01-10 18:07:36 -0500

    Initial Commit

---
+++ src/com/example/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,23 @@
+package com.example.remotecontrol;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Window;
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+	
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new RemoteControlFragment();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+	}
+	
+
+}
